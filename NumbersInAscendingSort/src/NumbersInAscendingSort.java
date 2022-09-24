e829da9cf5be2cab0cf413155e1c2b7c6b0f6c76
fatimaslan <113622954+fatimaslan@users.noreply.github.com>
2022-09-24 08:37:18 +0300

    Add files via upload

---
+++ NumbersInAscendingSort/src/NumbersInAscendingSort.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class NumbersInAscendingSort {
+
+	public static void main(String[] args) {
+		int a, b, c;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Üç sayı giriniz.");
+		System.out.print("1.Sayı: ");
+		a = input.nextInt();
+		System.out.print("2.Sayı: ");
+		b = input.nextInt();
+		System.out.print("3.Sayı: ");
+		c = input.nextInt();
+		
+		
+		
+		
+		
+		
+	}
+
+}
